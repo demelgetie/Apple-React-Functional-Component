2ef7335bdc51e980ebb21e74f4e97af4676492b6
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-10-27 00:19:54 +0300

    Ssection One of Apple react replica

--- react-apple/src/App.jsx
+++ react-apple/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import Header from './Components/Header/Header.jsx'
-import Footer from './Components/Footer/Footer.jsx'
-import AlertSection from './Components/Main_Body/AlertSection.jsx'
+import { useState } from "react";
+import Header from "./Components/Header/Header.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import AlertSection from "./Components/Main_Body/AlertSection.jsx";
+import SectionOne from "./Components/Main_Body/SectionOne.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <Header />
-     <Footer />
-     <AlertSection />
+      <Header />
+      <AlertSection />
+      <SectionOne />
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

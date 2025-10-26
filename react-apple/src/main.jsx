3ef9ee4d032f4854ce41a36148bd6ebb8af8ae18
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-10-26 22:36:28 +0300

    Header of apple react replica

--- react-apple/src/main.jsx
+++ react-apple/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import './assets/css/bootstrap.css'
+import './assets/css/styles.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

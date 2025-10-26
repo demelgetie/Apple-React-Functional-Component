dafa2af3a71ae2d138f1c4ddfc13cb6ea816791e
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-10-26 23:30:51 +0300

    Head finalized

--- react-apple/src/Components/Header/Header.jsx
+++ react-apple/src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../assets/images/icons/logo-sm.png";
 import search from "../../assets/images/icons/search-icon-sm.png";
-import cart from "../../assets/images/icons/cart.png";
+import cart from "../../assets/images/icons/cart-sm.png";
 
 function Header() {
   return (
